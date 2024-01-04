60370b362f4065740a6f40a2448a27001e4b849a
K. Miyawaki <kikusumk3@gmail.com>
2024-01-04 11:02:47 +0900

    :bug: fix file name (#5)

--- scripts/mib_profile/sw.py
+++ scripts/mib_profile/sw.py
@@ -89,7 +89,7 @@ def create_yamaha_sw():
         entity.entity_to_dict(
             {
                 "extends": [
-                    "yamaha_base.yml",
+                    "yamaha_base.yaml",
                 ],
                 "metadata": {
                     "interface": create_interface_meta(),
